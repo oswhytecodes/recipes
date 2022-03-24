38b244424a1e3520760c35f5c295bd1fee2bbc5d
Orincy Whyte <orincywhyte@gmail.com>
2022-03-23 23:04:40 -0400

    "march 23 commits"

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,20 +2,34 @@ import React, { useState } from "react";
 import './Header.css';
 import Menu from '../assets/images/menu.png'
 
+
 const Header = () => {
+  
+
+
+const showMenu = () => {
+    let menu = document.querySelector(".menu-burger");
+    let nav = document.querySelector(".nav-list");
+    
+  menu.classList.toggle('active');
+  nav.classList.toggle('active')
+}
+
+ 
+
     return (
     <nav>
      <h3 className="logo"> RECIPES </h3>
      <ul className="nav-list">
        <li><a href="#">Home</a> </li>
-       <li><a href="#">Pages</a> </li>
-       <li><a href="#">Elements</a></li>
-       <li><a href="#">Portfolio</a></li>
-       <li><a href="#">Blog</a></li>
+       <li><a href="#">Services</a> </li>
+       <li><a href="#">Recipes</a></li>
+       <li><a href="#">About Us</a></li>
+       <li><a href="#">Recipe Book</a></li>
        <li><a href="#">Shop</a></li>
        <li><a href="#">Search</a> </li>
      </ul>
-     <img className="menu-burger" src={Menu} alt="" />
+     <img onClick={showMenu} className="menu-burger" src={Menu} alt="" />
      </nav>
   ); 
   };

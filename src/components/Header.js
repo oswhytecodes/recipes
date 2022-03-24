41b30626cdf02549b6b9f01d9b13154c90754410
Orincy Whyte <orincywhyte@gmail.com>
2022-03-24 13:20:48 -0400

    header finally works

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,11 +3,20 @@ import "./Header.css";
 import Menu from "../assets/images/menu.png";
 
 const Header = () => {
+
   const showMenu = () => {
     let nav = document.querySelector(".nav-list");
 
     nav.classList.toggle("active");
   };
+  let hideMenu = () => {
+    let links = document.getElementsByTagName('li')
+    let nav = document.querySelector(".nav-list");
+
+   
+      nav.classList.toggle("active")
+  
+  }
 
   return (
     <nav>
@@ -15,25 +24,25 @@ const Header = () => {
         <h3> RECIPES </h3>
       </a>
       <ul className="nav-list">
-        <li>
+        <li onClick={hideMenu}>
           <a href="#Home">Home</a>
         </li>
-        <li>
+        <li onClick={hideMenu}>
           <a href="#Services">Services</a>
         </li>
-        <li>
+        <li onClick={hideMenu}>
           <a href="#Recipes">Recipes</a>
         </li>
-        <li>
+        <li onClick={hideMenu}>
           <a href="#About">About Us</a>
         </li>
-        <li>
+        <li onClick={hideMenu}>
           <a href="#Recipe_Book">Recipe Book</a>
         </li>
-        <li>
+        <li onClick={hideMenu}>
           <a href="#Shop">Shop</a>
         </li>
-        <li>
+        <li onClick={hideMenu}>
           <a href="#Search">Search</a>
         </li>
       </ul>
